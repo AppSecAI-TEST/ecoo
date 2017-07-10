c00c95112a3437a9fda63355b3593548f5ade569
Justin Rundle <RundleJ@aforbes.co.za>
2017-07-10 18:34:12 +0200

    SQL

--- db/src/main/resources/ecoo-mssql.sql
+++ db/src/main/resources/ecoo-mssql.sql
@@ -905,7 +905,7 @@ INSERT INTO "feature" ("id", "name", "value", "descr") VALUES
 (5, 'SMTP_PORT', '25', 'The SMTP server port'),
 (6, 'NON_PRODUCTION_EMAIL', 'justin@s-squared.co.za', 'The target email address for non-production deployments'),
 (7, 'OUTGOING_DISPLAY_NAME', 'ECOO Smart Admin', 'The display name when sending out emails out from the application'),
-(8, 'APPLICATION_ROOT_URL', 'http://testmyspivadmin.dedicated.co.za/ecoo-ui', 'The string representation of the URL to the application');
+(8, 'APPLICATION_ROOT_URL', 'http://testmyspivadmin.dedicated.co.za/ecoo-ui/', 'The string representation of the URL to the application');
 SET IDENTITY_INSERT "feature" OFF;
 
 CREATE TABLE "feature_log" (
