eaf862b1069f7f3cbe4823211633d51adb29b973
Justin Rundle <rundlej@aforbes.co.za>
2017-07-27 07:26:11 +0200

    NotificationResource send manual email.

--- gateway-api/src/main/java/ecoo/ws/system/rest/NotificationResource.java
+++ gateway-api/src/main/java/ecoo/ws/system/rest/NotificationResource.java
@@ -1,21 +1,27 @@
 package ecoo.ws.system.rest;
 
 import ecoo.data.Shipment;
-import ecoo.data.User;
 import ecoo.service.NotificationService;
 import ecoo.service.ShipmentService;
-import ecoo.service.UserService;
 import ecoo.ws.common.rest.BaseResource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.mail.*;
 import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.io.UnsupportedEncodingException;
+import java.net.InetAddress;
+import java.util.Date;
+import java.util.Properties;
 
 /**
  * @author Justin Rundle
@@ -25,23 +31,101 @@ import java.io.UnsupportedEncodingException;
 @RequestMapping("/api/notification")
 public class NotificationResource extends BaseResource {
 
+    private final static Logger LOG = LoggerFactory.getLogger(NotificationResource.class);
+
     private NotificationService notificationService;
 
     private ShipmentService shipmentService;
 
-@Autowired
+    @Autowired
     public NotificationResource(NotificationService notificationService, ShipmentService shipmentService) {
         this.notificationService = notificationService;
         this.shipmentService = shipmentService;
     }
 
     @RequestMapping(value = "/send/shipment/{shipmentId}", method = RequestMethod.GET)
-    public ResponseEntity<Boolean> sendTestEmail(@PathVariable Integer shipmentId) throws UnsupportedEncodingException, AddressException {
+    public ResponseEntity<Boolean> sendTestEmail(@PathVariable Integer shipmentId
+            , @PathVariable String smtpServer
+            , @PathVariable String smtpUser
+            , @PathVariable String smtpPwd
+            , @PathVariable String smtpDebug
+            , @PathVariable String smtpPort) throws UnsupportedEncodingException, AddressException {
         Shipment shipment = shipmentService.findById(shipmentId);
 
-        final MimeMessage newUserConfirmationEmail = notificationService.createShipmentNotification(shipment,"TEST");
-        notificationService.send(newUserConfirmationEmail);
-
+        final MimeMessage newUserConfirmationEmail = notificationService.createShipmentNotification(shipment, "TEST");
+        sendDirectly(newUserConfirmationEmail, smtpServer
+                , smtpUser
+                , smtpPwd
+                , smtpDebug
+                , smtpPort);
+        
         return ResponseEntity.ok(true);
+    }
+
+
+    @SuppressWarnings("Duplicates")
+    private void sendDirectly(MimeMessage mimeMessage, String smtpServer
+            , String smtpUser
+            , String smtpPwd
+            , String smtpDebug
+            , String smtpPort) {
+        Assert.notNull(mimeMessage);
+
+        final Properties properties = new Properties();
+        properties.setProperty("mail.transport.protocol", "smtp");
+        properties.setProperty("mail.host", smtpUser);
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.debug", smtpDebug);
+        properties.put("mail.smtp.port", smtpPort);
+
+        LOG.info(String.format("Using... {username=%s, password=%s}", smtpUser, smtpPwd));
+
+        final Session session = Session.getInstance(properties,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(smtpUser, smtpPwd);
+                    }
+                });
+        try {
+            final StringBuilder recipients = new StringBuilder();
+            for (Address a : mimeMessage.getRecipients(Message.RecipientType.TO)) {
+                recipients.append(((InternetAddress) a).getAddress());
+                recipients.append(" ");
+            }
+            LOG.info(String.format("smtp server set to %s -> attempting to send email to %s.",
+                    smtpServer.trim(), recipients));
+
+            if (mimeMessage.getSentDate() == null) {
+                mimeMessage.setSentDate(new Date());
+            }
+
+            final String outgoingDisplayName = InetAddress.getLocalHost().getHostName();
+            Address sender = new InternetAddress(smtpUser, outgoingDisplayName);
+            mimeMessage.setFrom(sender);
+
+            String messageId = mimeMessage.getMessageID();
+            mimeMessage.saveChanges();
+            if (messageId != null) {
+                mimeMessage.setHeader("Message-ID", messageId);
+            }
+
+            Transport transport = null;
+            try {
+                transport = session.getTransport();
+                transport.connect();
+                transport.sendMessage(mimeMessage, mimeMessage.getAllRecipients());
+            } finally {
+                if (transport != null) {
+                    try {
+                        transport.close();
+                    } catch (final Throwable t) {
+                        LOG.warn(t.getMessage(), t);
+                    }
+                }
+            }
+        } catch (final Throwable t) {
+            LOG.warn(t.getMessage(), t);
+        }
     }
 }
